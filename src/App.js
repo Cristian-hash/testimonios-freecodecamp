c0ebe4f61e7cb483f66559b24f1e03455383227d
Cristian-hash <cpacori@unsa.edu.pe>
2024-03-29 10:23:16 -0500

    2:21:02 Recibir props en un Componente

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Testimonio from "./componentes/Testimonio.js";
 function App() {
   return (
     <div className="App">
+      <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp</h1>
       <Testimonio />
     </div>
   );
