f7d3b906e55c9f446b6c6833b4d65aff0ef64f83
Cristian-hash <cpacori@unsa.edu.pe>
2024-03-29 10:37:01 -0500

    2:29:34 Personalizar la imagen

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../hojas-de-estilo/Testimonio.css";
 
-function Testimonio() {
+function Testimonio(props) {
   return (
     <div className="contenedor-testimonio">
       <img
@@ -10,15 +10,13 @@ function Testimonio() {
         alt="Foto de Emma"
       />
       <div className="contenedor-texto-testimonio">
-        <p className="nombre-testimonio">Emma Bostian en Suecia</p>
-        <p className="cargo-testimonio">Ingeniera de Software en Spotify</p>
-        <p className="texto-testimonio">
-          "I've always struggled with learning JavaScript. I've taken many
-          courses but freeCodeCamp's course was the one which stuck. Studying
-          JavaScript as well as data structures and algorithms on freeCodeCamp
-          gave me the skills and confidence I needed to land my dream job as a
-          software engineer at Spotify."
+        <p className="nombre-testimonio">
+          {props.nombre} en {props.pais}
         </p>
+        <p className="cargo-testimonio">
+          {props.cargo} en {props.empresa}
+        </p>
+        <p className="texto-testimonio">"{props.testimonio}"</p>
       </div>
     </div>
   );
